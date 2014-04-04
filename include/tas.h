7664dc901c7b3a738495fc82869b7fad751b8326
lplit <lplit@wp.pl>
2014-04-04 14:48:46 +0200

    Tas add

---
+++ include/tas.h
@@ -0,0 +1,47 @@
+#ifndef TAS
+#define TAS 
+
+typedef struct _elt { 
+  int num; 
+  double dist; 
+} Element; 
+
+typedef struct _tas { 
+  Element ** tab;
+  int nbEl; 
+  int tMax; 
+  int *indiceNoeud; 
+  int *places; 
+} Tas; 
+
+// Returns the pere of i
+int pere(int i);
+
+// If pere exists returns 1
+int hasPere(int i); 
+
+// Swaps i and j in t.
+void swap(Tas *t, int i, int j);
+
+// Brings it up
+void monter(Tas *t, int i);
+
+// Size
+int size(Tas * t);
+
+int isNoeud (Tas * t, int i);
+int isLeaf(Tas * t, int i);
+int left (int i);
+int right (int i);
+int hasRight(Tas * t, int i);
+int hasLeft(Tas *t, int i);
+int miniSon(Tas *t, int i);
+void descendre(Tas *t, int i);
+void insertTas(Tas *t, Element * add);
+Element * elMin(Tas *t);
+
+
+#endif
+
+
+
